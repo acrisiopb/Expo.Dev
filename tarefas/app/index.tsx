a31f2da45630821a564c5b516a2304decfafea85
Acrísio_Cruz <81418982+acrisiopb@users.noreply.github.com>
2025-01-26 01:55:48 -0300

    Aplicando grupo de estilos

--- tarefas/app/index.tsx
+++ tarefas/app/index.tsx
@@ -1,11 +1,59 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import React, {useState}from 'react';
 
 export default function App(){
+  const [nome, setNome] = useState("Lu");
+
+  function handleNome(){
+    // alert('test')
+    setNome("acrisio");
+  }
   return(
-    <View style={{flex:1, backgroundColor:'#121212', paddingTop: 28}}>
-        <Text style={{fontSize:28, color:"white"}}>Code + Coffe</Text>
-        <Text style={{color:"white"}}>Developer Full Stack</Text>
+    <View style={styles.container}>
+        <Text style={styles.title}>Code + Coffe</Text>
+        <Text style={[styles.title, styles.text]}>Developer Full Stack</Text>
+        <Text style={styles.nome}>{nome}</Text>
+
+        <TouchableOpacity style={styles.button} onPress={handleNome}>
+          <Text style={styles.buttonText}>Click</Text>
+        </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+     flex:1,
+     backgroundColor: '#f1f1f1',
+     paddingTop: 28,
+  },
+  title:{
+    fontSize:32,
+    color: "#121212",
+    fontWeight: 'bold'
+  }
+  ,
+  text:{
+    color: 'red'
+  },
+
+  nome:{
+    textAlign:'center',
+    fontSize:28,
+    fontStyle:'italic'
+  },
+
+  button:{
+    backgroundColor:'blue',
+    height: 40,
+    justifyContent: 'center',
+    alignItems:"center",
+    marginTop: 35
+  },
+
+  buttonText:{
+     color: '#FFF',
+     fontWeight: 'bold'
+  }
+
+})
