d51577fda4df1c293c4119834a9bf3eb0a6b07bb
Acrísio_Cruz <81418982+acrisiopb@users.noreply.github.com>
2025-01-26 00:51:33 -0300

    Entendendo a estrutura

---
+++ tarefas/app/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function App(){
+  return(
+    <View style={{flex:1, backgroundColor:'#121212', paddingTop: 28}}>
+        <Text style={{fontSize:28, color:"white"}}>Code + Coffe</Text>
+        <Text style={{color:"white"}}>Developer Full Stack</Text>
+    </View>
+  )
+}
