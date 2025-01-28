eceb9a9ca624edefcc6c1224f1e5825f39ec60f2
Acrísio_Cruz <81418982+acrisiopb@users.noreply.github.com>
2025-01-27 22:09:43 -0300

    Praticando - App Tarefas 3

--- tarefas/app/index.tsx
+++ tarefas/app/index.tsx
@@ -1,49 +1,78 @@
-import { StyleSheet, View, Text, TouchableOpacity, TextInput, FlatList } from 'react-native';
 import React, { useState } from 'react';
-import { FontAwesome } from '@expo/vector-icons';
-import Tarefa from './Tarefa';
-export default function App() {
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  FlatList
+} from 'react-native'
 
+import { FontAwesome } from '@expo/vector-icons'
+
+import Tarefa from '@/app/Tarefa'
+
+export default function App() {
   const [tarefa, setTarefa] = useState('');
 
-  const [list, setList] = useState([
-    {
-      key: '1',
-      item: "Notebook"
-    },
-    {
-      key: '2',
-      item: "Smatphone"
-    },
-    {
-      key: '3',
-      item: "HedSet"
-    },
-  ])
+  const [list, setList] = useState([])
+
 
   function handleAdd() {
-    alert(tarefa);
+
+    if (tarefa === '') {
+      return;
+    }
+
+    const dados = {
+      key: Date.now(),
+      item: tarefa
+    }
+
+    setList(oldArray => [dados, ...oldArray]);
+
+    setTarefa('')
+
   }
+
+
+  function handleDelete(item) {
+    let filtroItem = list.filter((tarefa) => {
+      return (tarefa.item !== item)
+    })
+
+    setList(filtroItem)
+
+  }
+
   return (
     <View style={styles.container}>
-
       <Text style={styles.title}>Tarefas</Text>
+
       <View style={styles.containerInput}>
-
-
-        <TextInput placeholder='Digite sua tarefa..' style={styles.input} value={tarefa} onChangeText={(text) => setTarefa(text)} />
+        <TextInput
+          placeholder="Digite sua tarefa..."
+          style={styles.input}
+          value={tarefa}
+          onChangeText={(text) => setTarefa(text)}
+        />
 
         <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
-          <FontAwesome name="plus" size={20} color="white" />
+          <FontAwesome name="plus" size={20} color="#FFF" />
         </TouchableOpacity>
       </View>
-          <FlatList style={styles.list}
-            data={list} keyExtractor={(item) => item.key}
-            renderItem={({ item }) => <Tarefa data={item} />}
-          />
+
+      <FlatList
+        data={list}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => <Tarefa data={item} deleteItem={() => handleDelete(item.item)} />}
+        style={styles.list}
+      />
+
     </View>
   )
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -53,46 +82,40 @@ const styles = StyleSheet.create({
   },
   title: {
     fontWeight: 'bold',
-    color: '#fff',
     fontSize: 24,
+    color: '#FFF',
     marginTop: '5%',
     paddingStart: '5%',
     marginBottom: 12,
   },
-
-  input: {
-    width: '75%',
-    backgroundColor: "#fbfbfb",
-    height: 44,
-    borderRadius: 4,
-    paddingHorizontal: 14,
-
-  },
-
   containerInput: {
     flexDirection: 'row',
-    width: "100%",
+    width: '100%',
     height: 44,
-    alignItems: "center",
+    alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 22,
-  }
-  ,
+  },
+  input: {
+    width: '75%',
+    backgroundColor: '#FBFBFB',
+    height: 44,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+  },
   buttonAdd: {
     width: '15%',
     height: 44,
     backgroundColor: '#73f7ff',
-    justifyContent: 'center',
+    marginLeft: 8,
     alignItems: 'center',
-    marginLeft: 5,
-    borderRadius: 5
+    justifyContent: 'center',
+    borderRadius: 4
   },
-
   list: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     paddingStart: '4%',
     paddingEnd: '4%'
-
   }
 })
